7cbbea035636e4e52d93a58bb9d3042ee7c1b934
Jesse P <jesseorhs@gmail.com>
2023-01-25 12:22:13 -0800

    Add playGame function to script.js

--- script.js
+++ script.js
@@ -14,4 +14,29 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function playGame(playerSelection, computerSelection = getComputerChoice()) {
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
+    if(playerChoice == 'rock' && computerChoice == 'scissors'){
+        winner = "You win! Rock beats Scissors";
+        return winner;
+    }
+    else if (playerChoice == 'paper' && computerChoice == 'rock'){
+        winner = "You win! Paper beats Rock";
+        return winner;
+    }
+    else if (playerChoice == 'scissors' && computerChoice == 'paper'){
+        winner = "You win! Scissors beats paper";
+        return winner;
+    }
+    else if (playerChoice == 'rock' && computerChoice == 'rock' || playerChoice == 'paper' && computerChoice == 'paper' || playerChoice == 'scissors' && computerChoice == 'scissors'){
+        winner = "It was a tie! Try again";
+        return winner;
+    }
+    else {
+        winner = "You lost! Try again";
+        return winner;
+    }
+}
+
+console.log(playGame('scissorS'))

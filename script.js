bd1526f0ae818797c6e5299227c4d928e2479b8d
Jesse P <jesseorhs@gmail.com>
2023-01-30 15:27:45 -0800

    Refactored code to allow DOM manipulation and allow game play through buttons

--- script.js
+++ script.js
@@ -18,62 +18,157 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     let playerChoice = playerSelection.toLowerCase();
     let computerChoice = computerSelection.toLowerCase();
     if(playerChoice == 'rock' && computerChoice == 'scissors'){
-        winner = "You win! Rock beats Scissors";
-        return winner;
+        playerScore++
     }
     else if (playerChoice == 'paper' && computerChoice == 'rock'){
-        winner = "You win! Paper beats Rock";
-        return winner;
+        playerScore++
     }
     else if (playerChoice == 'scissors' && computerChoice == 'paper'){
-        winner = "You win! Scissors beats paper";
-        return winner;
+         playerScore++
     }
     else if (playerChoice == 'rock' && computerChoice == 'rock' || playerChoice == 'paper' && computerChoice == 'paper' || playerChoice == 'scissors' && computerChoice == 'scissors'){
-        winner = "It was a tie! Try again";
-        return winner;
+        tieScore++
     }
-    else {
-        winner = "You lost! Try again";
-        return winner;
+    else{
+        computerScore++
+    }
+
+    if(playerScore == 5 || computerScore == 5){
+        replay.appendChild(replayContent);
     }
 }
 
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const results = document.querySelector('.results')
+const replay = document.querySelector('.replay')
+const replayContent = document.createElement('button')
+replayContent.textContent = 'Play again?'
 
-function game(){
-    playerScore = 0;
-    computerScore = 0;
-    tieScore = 0;
-    for (let i = 0; i < 5; i++){
-        playerResponse = prompt(`Lets play 5 rounds! Rock, paper, or scissors? \nRound: ${i} \nYour score: ${playerScore} \nComputer score: ${computerScore} \n Tie: ${tieScore}`);
-        playRound(playerResponse);
-        if(winner == "You lost! Try again"){
-            computerScore++
-        }
-        else if(winner == "It was a tie! Try again"){
-            tieScore++
-        }
-        else{
-            playerScore++
-        }
-    }
-    if(playerScore > computerScore){
-        playAgain = prompt(`You won! Score: ${playerScore} to ${computerScore}, play again?(Yes / No)`);
-        return playAgain;
-    }
-   else if(playerScore == computerScore) {
-        playAgain = prompt(`It's a tie! Score: ${playerScore} to ${computerScore}, Tie rounds: ${tieScore} play again?(Yes / No) `);
-        return playAgain;
-   } 
-    else {
-    playAgain = prompt(`You lost! Score: ${playerScore} to ${computerScore}, Play again?(Yes/No)`);
-        return playAgain;
-    } 
-}
+let playAgain = 'no';
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
 
-let playAgain = "yes"
-while (playAgain == "yes"){
-    game()
-    playAgain = playAgain.toLowerCase()
-}
-alert(`Thanks for playing!`)
+replayContent.addEventListener('click', () =>{
+    playAgain = 'yes'
+    if (playerScore >= 5 || computerScore >= 5 && playAgain == 'yes'){
+        playerScore = 0;
+        computerScore = 0;
+        tieScore = 0;
+        playAgain = 'no';
+        replay.removeChild(replayContent);
+        results.textContent = `Your score:${playerScore} \n 
+        Computer score: ${computerScore} \n
+       Tie Score: ${tieScore}`;
+    }
+})
+
+
+rock.addEventListener('click', () =>{
+    playRound('Rock')
+    results.textContent = `Your score:${playerScore} \n 
+ Computer score: ${computerScore} \n
+Tie Score: ${tieScore}`;
+})
+
+paper.addEventListener('click', () =>{
+   playRound('Paper')
+   results.textContent = `Your score:${playerScore} \n 
+ Computer score: ${computerScore} \n
+Tie Score: ${tieScore}`;
+})
+    
+scissors.addEventListener('click', () =>{
+    playRound('Scissors')
+    results.textContent = `Your score:${playerScore} \n 
+ Computer score: ${computerScore} \n
+Tie Score: ${tieScore}`;
+})
+
+
+results.textContent = `Your score:${playerScore} \n 
+ Computer score: ${computerScore} \n
+Tie Score: ${tieScore}`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let playAgain = "yes"
+    
+// while (playAgain == "yes"){
+//     game()
+//     playAgain = playAgain.toLowerCase()
+// }
+    
+// alert(`Thanks for playing!`)
+
+
+
+
+
+
+
+
+
+
+
+    // function game(){
+    //     playerScore = 0;
+    //     computerScore = 0;
+    //     tieScore = 0;
+    //     for (let i = 0; i < 5; i++){
+    //         playRound(playerResponse);
+    //         if(winner == "You lost! Try again"){
+    //             computerScore++
+    //         }
+    //         else if(winner == "It was a tie! Try again"){
+    //             tieScore++
+    //         }
+    //         else{
+    //             playerScore++
+    //         }
+    //     }
+    //     if(playerScore > computerScore){
+    //         playAgain = prompt(`You won! Score: ${playerScore} to ${computerScore}, play again?(Yes / No)`);
+    //         return playAgain;
+    //     }
+    //    else if(playerScore == computerScore) {
+    //        playAgain = prompt(`It's a tie! Score: ${playerScore} to ${computerScore}, Tie rounds: ${tieScore} play again?(Yes / No) `);
+    //        return playAgain;
+    //     } 
+    //     else {
+    //         playAgain = prompt(`You lost! Score: ${playerScore} to ${computerScore}, Play again?(Yes/No)`);
+    //         return playAgain;
+    //     } 
+    // }
+
+    // function game(){
+    //     playerScore = 0;
+    //     computerScore = 0;
+    //     tieScore = 0;
+
+    //     if(playerScore > computerScore){
+    //         playAgain = prompt(`You won! Score: ${playerScore} to ${computerScore}, play again?(Yes / No)`);
+    //         return playAgain;
+    //     }
+    //    else if(playerScore == computerScore) {
+    //        playAgain = prompt(`It's a tie! Score: ${playerScore} to ${computerScore}, Tie rounds: ${tieScore} play again?(Yes / No) `);
+    //        return playAgain;
+    //     } 
+    //     else {
+    //         playAgain = prompt(`You lost! Score: ${playerScore} to ${computerScore}, Play again?(Yes/No)`);
+    //         return playAgain;
+    //     } 
+    // }
